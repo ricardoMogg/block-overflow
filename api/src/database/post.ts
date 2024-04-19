241426a282bc7323ec04fb4eb6d4c393e99f66e7
Sam Luo <sam.luo@coinbase.com>
2024-04-18 20:37:14 -0500

    added commenting API and comment upvotes/downvotes

--- api/src/database/post.ts
+++ api/src/database/post.ts
@@ -2,15 +2,15 @@ import client from './client'
 
 interface CreatePostInput {
   title: string
-  body: string
-  authorId: string
+  content: string
+  walletAddress: string
 }
 function CreatePost(post: CreatePostInput) {
   return client.post.create({
     data: {
       title: post.title,
-      body: post.body,
-      authorId: post.authorId,
+      content: post.content,
+      walletAddress: post.walletAddress,
     },
   })
 }
@@ -19,6 +19,16 @@ function GetPost(id: string) {
   return client.post.findFirstOrThrow({
     where: {
       id: id,
+    },
+    include: {
+      comments: true,
+      upvotes: true,
+      _count: {
+        select: {
+          comments: true,
+          upvotes: true,
+        },
+      },
     },
   })
 }
@@ -29,8 +39,174 @@ function GetPosts() {
       orderBy: {
         createdAt: 'desc',
       },
-    }
+      include: {
+        _count: {
+          select: {
+            comments: true,
+            upvotes: true,
+          },
+        },
+      },
+    },
   )
 }
 
-export { CreatePost, GetPosts, GetPost, CreatePostInput }
+function GetComment(id: string) {
+  return client.comment.findFirstOrThrow({
+    where: {
+      id: id,
+    },
+    include: {
+      _count: {
+        select: {
+          upvotes: true,
+        },
+      },
+      }
+  })
+}
+
+interface CreateCommentInput {
+  content: string
+  walletAddress: string
+  postId: string
+}
+function CreateComment(comment: CreateCommentInput) {
+  return client.post.update({
+    where: {
+      id: comment.postId,
+    },
+    data: {
+      comments: {
+        create: {
+          content: comment.content,
+          walletAddress: comment.walletAddress,
+        },
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          comments: true,
+          upvotes: true,
+        },
+      }
+    }
+  })
+}
+
+interface UpvotePostInput {
+  postId: string
+  walletAddress: string
+}
+function UpvotePost(upvote: UpvotePostInput) {
+  return client.post.update({
+    where: {
+      id: upvote.postId,
+    },
+    data: {
+      upvotes: {
+        create: {
+          walletAddress: upvote.walletAddress,
+        },
+      }
+    },
+    include: {
+      _count: {
+        select: {
+          comments: true,
+          upvotes: true,
+        },
+      }
+    }
+  })
+}
+
+interface UpvoteCommentInput {
+  postId: string
+  commentId: string
+  walletAddress: string
+}
+function UpvoteComment(upvote: UpvoteCommentInput) {
+  return client.comment.update({
+    where: {
+      id: upvote.commentId,
+    },
+    data: {
+      upvotes: {
+        create: {
+          walletAddress: upvote.walletAddress,
+        }
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          upvotes: true,
+        },
+      }
+    }
+  })
+}
+
+interface DownvotePostInput {
+  postId: string
+  walletAddress: string
+}
+function DownvotePost(downvote: DownvotePostInput) {
+  return client.post.update({
+    where: {
+      id: downvote.postId,
+    },
+    data: {
+      upvotes: {
+        delete: {
+          postId_walletAddress: {
+            postId: downvote.postId,
+            walletAddress: downvote.walletAddress,
+          },
+        },
+      }
+    },
+    include: {
+      _count: {
+        select: {
+          comments: true,
+          upvotes: true,
+        },
+      }
+    }
+  })
+}
+
+interface DownvoteCommentInput {
+  postId: string
+  commentId: string
+  walletAddress: string
+}
+function DownvoteComment(downvote: DownvoteCommentInput) {
+  return client.comment.update({
+    where: {
+      id: downvote.commentId,
+    },
+    data: {
+      upvotes: {
+        delete: {
+          commentId_walletAddress: {
+            commentId: downvote.commentId,
+            walletAddress: downvote.walletAddress,
+          },
+        }
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          upvotes: true,
+        },
+      }
+    }
+  })
+}
+
+export { CreatePost, GetPosts, GetPost, CreatePostInput, CreateComment, CreateCommentInput, UpvotePost, UpvoteComment, UpvoteCommentInput, UpvotePostInput, GetComment, DownvoteCommentInput, DownvoteComment, DownvotePostInput, DownvotePost }
--- api/src/routes/post.ts
+++ api/src/routes/post.ts
@@ -1,18 +1,17 @@
 import { Router } from 'express';
-import { CreatePost, GetPosts, GetPost, CreatePostInput } from '../database/post';
+import { CreatePost, GetPosts, GetPost, CreatePostInput, CreateComment, CreateCommentInput, UpvotePost, UpvoteComment, UpvoteCommentInput, UpvotePostInput, DownvoteComment, DownvoteCommentInput, DownvotePost, DownvotePostInput } from '../database/post';
 
 export const postRouter = Router();
 
 // create post
 postRouter.post('/', async (req, res) => {
   try {
-    const {title, body, authorId} = req.body;
+    const {title, content, walletAddress} = req.body;
     const post = <CreatePostInput>{
       title,
-      body,
-      authorId,
+      content,
+      walletAddress,
     }
-    console.log(title, post, authorId)
     const posts = await CreatePost(post);
     return res.json(posts);
   } catch (e) {
@@ -43,6 +42,96 @@ postRouter.get('/', async (req, res) => {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+// add comment to post
+postRouter.post('/:id/comment', async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const {content, walletAddress} = req.body;
+    const comment = <CreateCommentInput>{
+      content: content,
+      walletAddress,
+      postId,
+    }
+    const posts = await CreateComment(comment);
+    return res.json(posts);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// upvote a post
+postRouter.post('/:id/upvote', async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const input = <UpvotePostInput>{
+      postId,
+      walletAddress: req.body.walletAddress,
+    }
+    const posts = await UpvotePost(input);
+    return res.json(posts);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// upvote a comment
+postRouter.post('/:id/comment/upvote/:commentId', async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const commentId = req.params.commentId;
+    const {walletAddress} = req.body;
+    const input = <UpvoteCommentInput>{
+      postId,
+      commentId,
+      walletAddress,
+    }
+    const posts = await UpvoteComment(input);
+    return res.json(posts);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// downvote a post
+postRouter.delete('/:id/downvote', async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const input = <DownvotePostInput>{
+      postId,
+      walletAddress: req.body.walletAddress,
+    }
+    const posts = await DownvotePost(input);
+    return res.json(posts);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// downvote a comment
+postRouter.delete('/:id/comment/downvote/:commentId', async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const commentId = req.params.commentId;
+    const {walletAddress} = req.body;
+    const input = <DownvoteCommentInput>{
+      postId,
+      commentId,
+      walletAddress,
+    }
+    const posts = await DownvoteComment(input);
+    return res.json(posts);
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 // sanity check
 postRouter.post('/health', (req, res) => {
