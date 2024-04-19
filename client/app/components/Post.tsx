a6ae787dd75f43785aece1a9d4f3a4f4bdbddbc9
Michael Chan <michael.chan@coinbase.com>
2024-04-18 17:21:06 -0700

    feat: post metrics

--- client/app/components/Post.tsx
+++ client/app/components/Post.tsx
@@ -1,51 +1,82 @@
-import { HStack, VStack, Text, Box, Image } from "@chakra-ui/react";
+import { HStack, VStack, Text, Box, Image, Badge } from "@chakra-ui/react";
 import { memo } from "react";
 
-export type Post = {
+export type Post = PostDetail & PostMetrics;
+
+export type PostDetail = {
   userId: number;
   id: number;
   title: string;
   body: string;
 };
 
-const PostComponent = memo(function PostComponent(post: Post) {
+export type PostMetrics = {
+  voteCount: number;
+  answerCount: number;
+  viewCount: number;
+  reward?: {
+    amount: number;
+    currency: string;
+  };
+};
+
+const PostDetail = memo(function PostDetail({
+  title,
+  body,
+  userId,
+}: PostDetail) {
   return (
-    <HStack>
-      <PostDetail {...post} />
+    <HStack alignItems="flex-start" gap="16px">
+      <Box w="32px" borderRadius="16px" overflow="hidden" flexShrink="0">
+        <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+      </Box>
+      <VStack align="flex-start" gap="2px">
+        <Text fontSize="large">{title}</Text>
+        <Text fontSize="small">{`By: ${userId}`}</Text>
+        <Text fontSize="medium" color="GrayText">
+          {body}
+        </Text>
+      </VStack>
     </HStack>
   );
 });
 
-const PostDetail = memo(function PostDetail({ title, body, userId }: Post) {
+function PostMetricsBase(props: PostMetrics) {
   return (
-    <HStack
-      alignItems="flex-start"
-      gap="16px"
-    >
-      <Box
-        w="32px"
-        borderRadius="16px"
-        overflow="hidden"
-        flexShrink="0"
-      >
-        <Image
-          src="https://bit.ly/dan-abramov"
-          alt="Dan Abramov"
-        />
-      </Box>
-      <VStack
-        align="flex-start"
-        gap="2px"
-      >
-        <Text fontSize="large">{title}</Text>
-        <Text fontSize="small">{`By: ${userId}`}</Text>
-        <Text
-          fontSize="medium"
-          color="GrayText"
-        >
-          {body}
+    <VStack alignItems="flex-start">
+      <VStack alignItems="flex-start" color="#5B616E" gap={1}>
+        <Text as="span" whiteSpace="nowrap">
+          0 votes
+        </Text>
+        <Text as="span" whiteSpace="nowrap">
+          0 answers
+        </Text>
+        <Text as="span" whiteSpace="nowrap">
+          0 views
         </Text>
       </VStack>
+      <Badge
+        variant="solid"
+        color="#098551"
+        fontWeight={600}
+        backgroundColor="#F5FFFB"
+        borderRadius={4}
+        px={4}
+      >
+        1 ETH reward
+      </Badge>
+    </VStack>
+  );
+}
+
+const PostComponent = memo(function PostComponent(post: Post) {
+  const { voteCount, answerCount, viewCount, reward, ...details } = post;
+  const metrics = { voteCount, answerCount, viewCount, reward };
+
+  return (
+    <HStack alignItems="flex-start">
+      <PostDetail {...details} />
+      <PostMetricsBase {...metrics} />
     </HStack>
   );
 });
