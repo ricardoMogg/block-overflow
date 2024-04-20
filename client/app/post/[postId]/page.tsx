1f72fc862d77f7f2e798fc9ee68d165b07ecd82b
Sam Luo <sam.luo@coinbase.com>
2024-04-20 12:01:16 -0500

    unstyled view post page

--- client/app/post/[postId]/page.tsx
+++ client/app/post/[postId]/page.tsx
@@ -7,6 +7,7 @@ import {
   Flex,
   HStack,
   Heading,
+  Icon,
   Stack,
   Tag,
   Text,
@@ -35,6 +36,11 @@ type Post = {
   bountyId?: string;
   bountyAmount?: number;
   bountyStatus?: string;
+  _count: {
+    comments: number;
+    upvotes: number;
+  };
+  createdAt: Date;
 };
 
 export default function CreatePage({ params }: { params: { postId: string } }) {
@@ -70,53 +76,87 @@ export default function CreatePage({ params }: { params: { postId: string } }) {
   return (
     <main className="flex min-h-screen flex-col justify-between">
       <HeaderContainer></HeaderContainer>
-      <HStack
-        maxW={1080}
+      <VStack
+        maxW="800px"
         flex={1}
         gap={40}
         alignItems="flex-start"
         alignSelf="center"
       >
         <Box
-          maxWidth="800px"
-          margin="auto"
-          mt={5}
-          p={5}
-          borderWidth="1px"
-          borderRadius="md"
-          borderColor="gray.200"
+          bgColor={"green/0"}
+          color="green"
+          borderColor={"green/5"}
+          borderWidth={"1px"}
+          borderRadius={"8px"}
+          gap={40}
+          w="100%"
         >
-          <Heading as="h2" size="xl" mb={2}>
-            {post?.title}
-          </Heading>
-          <Text fontSize="md" color="gray.500" mb={4}>
-            {post?.walletAddress}
-          </Text>
-          <Text mb={4}>{post?.content}</Text>
-          <Divider my={4} />
-          <Stack direction="row" spacing={2} mb={2}>
-            {post?.tags.map((tag, index) => (
-              <Tag key={index} colorScheme="blue" borderRadius="full">
-                {tag}
-              </Tag>
-            ))}
-          </Stack>
-          {post?.bountyId && (
-            <Flex alignItems="center" justifyContent="space-between">
-              <Badge
-                colorScheme={getBountyStatusColorScheme(post?.bountyStatus)}
-                px={2}
-                py={1}
-              >
-                {formatBounty(post?.bountyAmount)} - {post?.bountyStatus}
-              </Badge>
-              <Text fontSize="sm" color="gray.500">
-                Bounty ID: {post?.bountyId}
+          <HStack>
+            <Icon name="fa-info" color="green.500" />
+            <VStack spacing={4} alignItems="flex-start">
+              <Text as="h1" size="xxl">
+                {formatBounty(post?.bountyAmount)} tip
               </Text>
-            </Flex>
-          )}
+              <Text as="h1" size="xl">
+                The poster of this question has attached a{" "}
+                {formatBounty(post?.bountyAmount)} tip for the best answer
+              </Text>
+            </VStack>
+          </HStack>
         </Box>
-      </HStack>
+        <HStack>
+          <Box>
+            <VStack>
+              <Icon name="TriangleUpIcon" color="green.500" />
+              <Text>{post?._count?.upvotes}</Text>
+              <Icon name="TriangleDownIcon" color="green.500" />
+            </VStack>
+          </Box>
+          <Box>
+            <VStack spacing={4} alignItems="flex-start">
+              <Heading as="h2" size="xl" mb={2} fontWeight="bold">
+                {post?.title}
+              </Heading>
+              <Text mb={4}>{post?.content}</Text>
+              <HStack>
+                {post?.tags.map((tag) => (
+                  <Tag key={tag}>{tag}</Tag>
+                ))}
+              </HStack>
+            </VStack>
+          </Box>
+          <Box>
+            <VStack>
+              <Text>
+                Posted on{" "}
+                {post?.createdAt
+                  ? new Date(post?.createdAt).toLocaleDateString()
+                  : new Date().toLocaleDateString()}{" "}
+                by:
+              </Text>
+              <Text>{post?.walletAddress}</Text>
+              <HStack>
+                <Button>Edit question</Button>
+                <Button>Share as a Frame</Button>
+              </HStack>
+            </VStack>
+          </Box>
+        </HStack>
+        <Text fontWeight={"bold"}>Answers</Text>
+
+        <Box
+          height="30vh"
+          padding="0 20px"
+          backgroundImage="url(/no_comments.png)"
+          backgroundSize="cover"
+          backgroundPosition="center"
+          width="100%"
+        ></Box>
+        <Text padding="0 20px">
+          There are no responses yet, be the first to answer
+        </Text>
+      </VStack>
     </main>
   );
 }
