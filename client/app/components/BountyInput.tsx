aef11ecda464bb2c23fcf7fc674a47e755fb83ce
Ricardo Moguel <ricardo.mogg@gmail.com>
2024-04-19 23:14:43 -0700

    Fix dynamics omports

---
+++ client/app/components/BountyInput.tsx
@@ -0,0 +1,71 @@
+import {
+  Box,
+  Button,
+  HStack,
+  Image,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import { useRouter } from "next/navigation";
+
+import { memo } from "react";
+
+function BountyInput({ updateAmount }: { updateAmount: () => number }) {
+  return (
+    <Box
+      width="100%"
+      padding={20}
+      borderRadius={8}
+      borderColor="#5B616EA8"
+      borderWidth={1}
+      pt={0}
+    >
+      <HStack
+        marginTop={20}
+        flex={1}
+        gap={40}
+        alignItems="flex-start"
+        alignSelf="center"
+      >
+        <VStack maxW="40%" gap={24} alignItems="flex-start">
+          <Input
+            w="100%"
+            fontSize={32}
+            placeholder="0.00"
+            onChange={() => {
+              updateAmount();
+            }}
+          />
+        </VStack>
+        <VStack alignItems="flex-end">
+          <Button
+            leftIcon={
+              <Image
+                boxSize="20px"
+                src="https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png"
+              />
+            }
+            backgroundColor="#EEF0F3"
+            color="#0A0B0D"
+            borderRadius={20}
+            px={12}
+            py={8}
+            paddingRight={30}
+            paddingLeft={20}
+            fontSize={12}
+            fontWeight={800}
+          >
+            ETH
+          </Button>
+          <Text color="#aaa" fontSize="12" fontWeight={600}>
+            Balance: 0 ETH
+          </Text>
+        </VStack>
+      </HStack>
+    </Box>
+  );
+}
+
+export default memo(BountyInput);
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,30 +1,35 @@
-"use client";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+
+import {
+  DynamicContextProvider,
+  EthereumWalletConnectors,
+  DynamicWagmiConnector,
+} from "../lib/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Block overflow",
+  description: "Don't be scared to ask anon.",
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <DynamicContextProvider
-          settings={{
-            // Find your environment id at https://app.dynamic.xyz/dashboard/developer
-            environmentId: "f70aa699-4197-4c47-bede-c2e8cf23f927",
-            walletConnectors: [EthereumWalletConnectors],
-          }}
-        >
-          {children}
-        </DynamicContextProvider>
-      </body>
+      <DynamicContextProvider
+        settings={{
+          environmentId: "f70aa699-4197-4c47-bede-c2e8cf23f927",
+          walletConnectors: [EthereumWalletConnectors],
+        }}
+      >
+        <body className={inter.className}>{children}</body>
+      </DynamicContextProvider>
     </html>
   );
 }
