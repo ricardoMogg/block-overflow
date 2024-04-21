9e74ef24c610cf5d2869374b3a068a453fd236d9
Mark Hao <mark.hao@coinbase.com>
2024-04-20 17:00:10 -0700

    styled create page

--- client/app/create/page.tsx
+++ client/app/create/page.tsx
@@ -1,13 +1,6 @@
 "use client";
-import {
-  Box,
-  Button,
-  Flex,
-  HStack,
-  Text,
-  Textarea,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Button, HStack, Text, Textarea, VStack } from "@chakra-ui/react";
+import { FormControl, FormLabel, FormHelperText } from "@chakra-ui/react";
 import Input from "../components/shared/Input";
 import HeaderContainer from "../components/Header/HeaderContainer";
 import { CreatePost } from "../hooks/post";
@@ -20,7 +13,10 @@ import abi from "../../lib/BountyContract.json";
 
 function RequiredIndicator() {
   return (
-    <Text color="#CF202F" as="span">
+    <Text
+      color="#CF202F"
+      as="span"
+    >
       *
     </Text>
   );
@@ -69,7 +65,7 @@ export default function CreatePage() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col justify-between">
+    <main className="flex min-h-screen flex-col ">
       <HeaderContainer>
         <Text
           color="white"
@@ -80,25 +76,32 @@ export default function CreatePage() {
           Don’t be scared to ask anon.
         </Text>
       </HeaderContainer>
-      <HStack flex={1} gap={40} alignItems="flex-start" alignSelf="center">
-        <VStack gap={24} alignItems="flex-start" maxW={1640} w="100%">
-          <form action={onSubmit}>
-            <label style={{ width: "100%" }}>
-              <Text fontWeight={500}>
-                Title
-                <RequiredIndicator />
-              </Text>
-              <Input placeholder="How do I..." width="100%" name="title" />
-            </label>
-            <label style={{ width: "100%" }}>
-              <Text fontWeight={500}>
-                Question body
-                <RequiredIndicator />
-              </Text>
+      <HStack
+        flex={1}
+        gap={10}
+        width="60%"
+        alignItems="flex-start"
+        alignSelf="center"
+        paddingTop={20}
+      >
+        <form
+          action={onSubmit}
+          style={{ width: "100%" }}
+        >
+          <VStack>
+            <FormControl isRequired>
+              <FormLabel fontWeight={500}>Title</FormLabel>
+              <Input
+                name="title"
+                type="text"
+                placeholder="How do I..."
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Question body</FormLabel>
               <Box
                 borderRadius={8}
                 borderColor="#5B616EA8"
-                borderWidth={1}
                 pt={0}
               >
                 <Textarea
@@ -108,44 +111,41 @@ export default function CreatePage() {
                   width="100%"
                 />
               </Box>
-            </label>
-            <label style={{ width: "100%" }}>
-              <Text fontWeight={500}>
-                Tags
-                <RequiredIndicator />
-              </Text>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel fontWeight={500}>Tags</FormLabel>
               <Input
                 name="tags"
                 placeholder="Tag chains and technologies you are using to make it easier for others to find"
-                width="100%"
               />
-            </label>
-            {isAuthenticated ? (
-              <Button
-                type="submit"
-                w={327}
-                color="#fff"
-                backgroundColor="#0052FF"
-                borderRadius={100}
-                px={32}
-                py={16}
-                fontWeight={500}
-                marginTop={20}
-                disabled={!isAuthenticated}
-              >
-                Post your question
-              </Button>
-            ) : (
-              <DynamicWidget />
-            )}
-          </form>
-        </VStack>
-        <VStack maxW={300} gap={24}>
+            </FormControl>
+          </VStack>
+        </form>
+        <VStack
+          maxW={300}
+          alignItems="stretch"
+          gap={6}
+        >
           <VStack alignItems="flex-start">
             <Text fontWeight={500}>{`Add a tip?`}</Text>
             <Text color="#5B616E">{`Bounties greatly increase the chance of your question being answered quickly. And it\’s just nice to tip anons willing to help you.`}</Text>
           </VStack>
           <BountyInput updateAmount={setBounty} />
+          {isAuthenticated ? (
+            <Button
+              type="submit"
+              color="#fff"
+              backgroundColor="#0052FF"
+              borderRadius={100}
+              fontWeight={500}
+              height="56px"
+              disabled={!isAuthenticated}
+            >
+              Post your question
+            </Button>
+          ) : (
+            <DynamicWidget />
+          )}
         </VStack>
       </HStack>
     </main>
